208704ec4ff8c45d8927180dbcb915eb212fa8d7
antitree <antitree@gmail.com>
2017-09-08 14:51:46 -0400

    adding standard syscalls and support for sysdig

---
+++ syscall2seccomp.py
@@ -0,0 +1,74 @@
+#!/usr/local/python
+# Script to convert strace output to
+# a docker seccomp JSON file format
+
+import fileinput
+import json
+import argparse
+
+import syscalls
+
+SECCOMP_PROFILE_TEMPLATE_PATH = "seccomp.json"
+
+# These syscalls are _required_ to start a container
+# see https://github.com/moby/moby/issues/22252
+BASE_SYSCALLS = syscalls.DOCKER
+
+
+def main(): 
+    parser = argparse.ArgumentParser(
+        description="Search for syscalls stored inside of an strace log and convert it to a Docker seccomp JSON profile" )
+    parser.add_argument("strace", 
+            help="Path to file containing the output from running strace on an application")
+    #parser.add_argument("-o", help="Optional output path")
+    parser.add_argument("-s", dest="sysdig", action="store_true", help="For handling sysdig output instead of strace")
+    args = parser.parse_args()
+    app_syscalls = set()
+
+    with fileinput.input(files=(args.strace)) as f:
+        if args.sysdig:
+            app_syscalls.update((x.replace('>','<').split(' < ', 1)[1].split(' ')[0] for x in f))
+        else: 
+            app_syscalls.update((x.split('(', 1)[0] for x in f if x[0].isalpha()))
+    
+    app_syscalls.intersection_update(syscalls.SYSCALLS)
+    to_profile(app_syscalls)
+
+
+def _load_template():
+    try:
+        with open(SECCOMP_PROFILE_TEMPLATE_PATH, 'r') as template:
+            template = json.loads(template.read())
+        return template
+    except:
+        print("Error importing template profile at: {}".format(SECCOMP_PROFILE_TEMPLATE_PATH))
+        return False
+
+
+def _syscall_template():
+    return json.loads('{"name": "","action": "SCMP_ACT_ALLOW","args": []}')
+
+
+def to_profile(syscalls):
+    template = _load_template()  # load json as dict
+    syscalls.update(BASE_SYSCALLS)  # required syscalls
+    
+    # calls each have a block
+    """
+    for call in syscalls:
+        newsyscall = _syscall_template()
+        newsyscall["names"] = call
+        template["syscalls"].append(newsyscall)
+    """
+
+    # calls are in one block
+    newsyscall = _syscall_template()
+    newsyscall["names"] = list(syscalls)
+    template["syscalls"].append(newsyscall)
+
+
+    print(json.dumps(template, indent=4))
+        
+    
+if __name__ == "__main__":
+    main()
